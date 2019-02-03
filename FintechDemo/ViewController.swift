19884b4306140a0bfa8bfd2af47dfbd0c2d411c3
enieto <enieto@ext.vectoritcgroup.com>
2019-02-03 13:26:39 +0100

    Initial Commit

---
+++ FintechDemo/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  FintechDemo
+//
+//  Created by Eduardo Nieto on 03/02/2019.
+//  Copyright © 2019 Eduardo Nieto. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
